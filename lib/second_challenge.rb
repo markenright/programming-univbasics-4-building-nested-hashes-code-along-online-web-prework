dea854ea98aa0ab51a9d4c0c0c7004be23aea952
Mark Enright <enright.mark@gmail.com>
2019-08-08 16:19:04 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,15 +3,20 @@ def second_challenge
     :montague => {
       #your key/value pairs here!
       {
-        :patriarch
-        :matriarch
-        :hero
+        :patriarch => {}
+        :matriarch => {}
+        :hero => {}
+        :hero_friends => []
         
       }
       
       }, 
     :capulet => {
       #your key/value pairs here!
+        :patriarch => {}
+        :matriarch => {}
+        :heroine => {}
+        :heroine_friends => []
       }}
 
   
